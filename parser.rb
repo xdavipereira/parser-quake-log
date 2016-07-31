61a9e0ee44317aba4f4d34a5eba0bc361604e0b5
Davi Pereira <xdavipereira@gmail.com>
2016-07-31 17:20:12 -0300

    Criando metodo para identificar a linha de novo jogo

--- parser.rb
+++ parser.rb
@@ -1,13 +1,23 @@
 class Parser
+	
 	def initialize
-		
+
 		@file = File.open("games.log", "r")
 
-		file.each do |f|
+		@file.each do |f|
 
-			puts f
+			if linha_novo_jogo?(f)
+				puts f + "\n"
+			end
 			
 		end
+	end
+
+
+	private
+
+	def linha_novo_jogo?(linha)
+		linha.match(/(?:^|\W)InitGame(?:$|\W)/) ? true : false
 	end
 
 end
