2b8129afe9fadc3580eadbcb2cb4ed7026bfd2dc
Davi Pereira <xdavipereira@gmail.com>
2016-07-31 18:50:51 -0300

    Criando menu para exibir as informações do parser

--- parser.rb
+++ parser.rb
@@ -107,11 +107,41 @@ class Parser
 end
 
 
-parser = Parser.new
+		puts "================================================="
+		puts "================================================="
+		puts "================== PARSER LOG ==================="
+		puts "O que você deseja ver?"
+		puts "\n\n"
+		puts "Digite 1 para ver informções sobre determinado jogo."
+		puts "Digite 2 para ver o ranking de kills e as principais causas dos abates por jogo."
+		puts "Digite 3 para ver o ranking geral de kills dos players."
 
-parser.ranking_jogo
-#parser.ranking_geral
-# parser.jogos.each do |j|
-# 	puts j.total_kills
-# 	puts j.players.map { |key, value| "Player: #{key} #{value.score} Kills" }
-# end
+		opcao = gets
+			
+		puts `clear`
+		
+
+		parser = Parser.new
+		
+		case opcao.to_i
+
+		when 1
+			puts "Houveram #{parser.jogos.length} jogos, você deseja ver as informações de qual jogo?"
+			puts "Escolha um numero entre 1 e #{parser.jogos.length}?"
+			jogo = gets
+
+			puts `clear`
+
+			puts "Informações sobre o jogo #{jogo.to_i}"
+			parser.informacao_do_jogo(jogo.to_i)
+
+		when 2
+			parser.relatorio_dos_jogos
+		when 3
+			parser.ranking_geral_de_abates
+		else
+			puts %{Escolha uma das tres opções:
+			'1' para informações de um determinado jogo.
+			'2' para ranking de kills e as causas dos abates por jogo.
+			'3' para ranking geral de kills dos players.}
+		end
