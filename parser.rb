031afccc77b4b34f50955c753491df987763c19a
Davi Pereira <xdavipereira@gmail.com>
2016-07-31 17:24:00 -0300

    Adicionando metodos para identificar as linhas de usuario, kill, e fim de jogo

--- parser.rb
+++ parser.rb
@@ -16,9 +16,24 @@ class Parser
 
 	private
 
-	def linha_novo_jogo?(linha)
-		linha.match(/(?:^|\W)InitGame(?:$|\W)/) ? true : false
-	end
+		def linha_novo_jogo?(linha)
+			linha.match(/(?:^|\W)InitGame(?:$|\W)/) ? true : false
+		end
+
+
+		def linha_usuario?(linha)
+			linha.match(/(?:^|\W)ClientUserinfoChanged(?:$|\W)/) ? true : false
+		end
+
+
+		def linha_fim_jogo?(linha)
+			linha.match(/(?:^|\W)ShutdownGame(?:$|\W)/) ? true : false
+		end
+
+
+		def linha_kill?(linha)
+			linha.match(/(?:^|\W)Kill(?:$|\W)/) ? true : false
+		end
 
 end
 
